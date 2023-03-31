e7ba9ab4a8ff8a43721e93d1839770d1a06bf039
Cisco248 <129523905+Cisco248@users.noreply.github.com>
2023-03-31 19:42:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let sections = document.querySelectorAll('selection');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if() {
+
+        }
+    });
+}
